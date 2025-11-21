3ce673d9fcb1e3f241e28dbf83dae3a19f9e2e96
basseychrist <enobongetimbassey101gmail.com>
2025-11-21 19:19:22 +0100

    feat: implement authentication check and redirect in Layout component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,15 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { redirect } from 'next/navigation';
+import { auth } from '@/auth';
+
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const session = await auth();
+
+  // If there's no authenticated user, redirect to the login page.
+  if (!session?.user) {
+    redirect('/login');
+  }
+
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
